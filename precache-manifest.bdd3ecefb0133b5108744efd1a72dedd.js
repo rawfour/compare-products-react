29745824ca626e67be9457f37408e50b11b40757
rawfour <rafaldziedziela@outlook.com>
2020-02-07 17:57:24 +0100

    Updates

---
+++ precache-manifest.bdd3ecefb0133b5108744efd1a72dedd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280ddddb2c5ca15e47b7b12ed7fcf2d4",
+    "url": "/compare-products-react/index.html"
+  },
+  {
+    "revision": "e621e7a1d02f88a79960",
+    "url": "/compare-products-react/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "fc9bef4a76831e0054db",
+    "url": "/compare-products-react/static/css/main.d2134f2b.chunk.css"
+  },
+  {
+    "revision": "e621e7a1d02f88a79960",
+    "url": "/compare-products-react/static/js/2.c321ebc4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/compare-products-react/static/js/2.c321ebc4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc9bef4a76831e0054db",
+    "url": "/compare-products-react/static/js/main.083018ef.chunk.js"
+  },
+  {
+    "revision": "f4fb1b27fd3d6fc60f1b",
+    "url": "/compare-products-react/static/js/runtime-main.cee69a6a.js"
+  }
+]);
